ca1b408ef583eff74bfc497f552a0fd0d8237a02
oteto <58469602+oteto@users.noreply.github.com>
2022-10-28 13:06:46 +0900

    impl tcp three way handshake (#1)

--- open_port.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo ip netns exec host2 nc -l 10.0.1.1 40000
--- src/tcp.rs
+++ src/tcp.rs
@@ -180,6 +180,81 @@ impl TCP {
         }
     }
 
+    /// LISTEN 状態のソケットに到着したパケットの処理
+    fn listen_handler(
+        &self,
+        mut table: RwLockWriteGuard<HashMap<SockID, Socket>>,
+        listening_socket_id: SockID,
+        packet: &TCPPacket,
+        remote_addr: Ipv4Addr,
+    ) -> Result<()> {
+        dbg!("listen handler");
+        if packet.get_flag() & tcpflags::ACK > 0 {
+            // 本来なら RST を send する
+            return Ok(());
+        }
+
+        let listening_socket = table.get_mut(&listening_socket_id).unwrap();
+        if packet.get_flag() & tcpflags::SYN > 0 {
+            // passive open の処理
+            // 後に接続済みソケットとなるソケットを新たに生成する
+            let mut connection_socket = Socket::new(
+                listening_socket.local_addr,
+                remote_addr,
+                listening_socket.local_port,
+                packet.get_src(),
+                TcpStatus::SynRcvd,
+            )?;
+            connection_socket.recv_param.next = packet.get_seq() + 1;
+            connection_socket.recv_param.initial_seq = packet.get_seq();
+            connection_socket.send_param.initial_seq = rand::thread_rng().gen_range(1..1 << 31);
+            connection_socket.send_param.window = packet.get_window_size();
+            connection_socket.send_tcp_packet(
+                connection_socket.send_param.initial_seq,
+                connection_socket.recv_param.next,
+                tcpflags::SYN | tcpflags::ACK,
+                &[],
+            )?;
+            connection_socket.send_param.next = connection_socket.send_param.initial_seq + 1;
+            connection_socket.send_param.unacked_seq = connection_socket.send_param.initial_seq;
+            connection_socket.listening_socket = Some(listening_socket.get_sock_id());
+            dbg!("status: listen -> ", &connection_socket.status);
+            table.insert(connection_socket.get_sock_id(), connection_socket);
+        }
+
+        Ok(())
+    }
+
+    fn synrcvd_handler(
+        &self,
+        mut table: RwLockWriteGuard<HashMap<SockID, Socket>>,
+        sock_id: SockID,
+        packet: &TCPPacket,
+    ) -> Result<()> {
+        dbg!("synrcvd handler");
+        let socket = table.get_mut(&sock_id).unwrap();
+
+        let ack = packet.get_ack();
+
+        if packet.get_flag() & tcpflags::ACK > 0
+            && socket.send_param.unacked_seq <= ack
+            && ack <= socket.send_param.next
+        {
+            socket.recv_param.next = packet.get_seq();
+            socket.send_param.unacked_seq = ack;
+            socket.status = TcpStatus::Established;
+            dbg!("status: synrcvd -> ", &socket.status);
+
+            if let Some(id) = socket.listening_socket {
+                let ls = table.get_mut(&id).unwrap();
+                ls.connected_connection_queue.push_back(sock_id);
+                self.publish_event(ls.get_sock_id(), TCPEventKind::ConnectionComplated);
+            }
+        }
+
+        Ok(())
+    }
+
     /// SYNSENT 状態のソケットに到着したパケットの処理
     fn synsent_handler(&self, socket: &mut Socket, packet: &TCPPacket) -> Result<()> {
         dbg!("synsent handler");
